3d13e571f5b63d1d293fc01b28ddda5a54660a9a
Amanda <80652875+Amanda641@users.noreply.github.com>
2022-03-14 12:57:24 -0300

    Add files via upload

---
+++ questao2.c
@@ -0,0 +1,17 @@
+/*Faça um programa que recebe um número e então imprima: “O número informado foi <número>.*/
+
+#include <stdio.h>
+
+int main(){
+
+    int num;
+
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    printf("O numero informado foi %d", num);
+
+
+
+
+}
